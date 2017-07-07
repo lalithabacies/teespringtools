f1099d600d2b2d99d5a9f12ab6ee3e0b416ee482
dencyabacies <dency@abacies.com>
2017-07-07 19:05:49 +0530

    sql file

---
+++ _private/sql/tshirt_users.sql
@@ -0,0 +1,13 @@
+CREATE TABLE tshirt_videos (
+    
+id integer NOT NULL,
+    
+videotitle character varying(100),
+    
+emdedurl text,
+    
+createdon timestamp without time zone,
+    
+status integer
+
+);
